26f4ad2fd1b0637a84bb156e3d80ef59c64bc023
MakarimZufar <makarim.zufar@gmail.com>
2024-11-09 19:38:11 +0700

    add form sample

---
+++ form_sample/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\Users\makar\dev\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\makar\Documents\ngampus\Semester 3\PBP D\Tugas\Individu\Tugas 7 dst\second_chance_mobile\form_sample"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
